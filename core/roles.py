13a7b6c45fe3a93228a4adc9b16c5f85d99513db
ragini19854-prog <ragini.19854@gmail.com>
2026-04-23 09:39:29 +0530

    Update roles.py

--- core/roles.py
+++ core/roles.py
@@ -1,1 +1,9 @@
+from bot.config import OWNER_ID
+from bot.database.db import managers_col
 
+async def is_manager(user_id):
+    user = await managers_col.find_one({"user_id": user_id})
+    return user is not None
+
+async def is_admin(user_id):
+    return user_id == OWNER_ID or await is_manager(user_id)
