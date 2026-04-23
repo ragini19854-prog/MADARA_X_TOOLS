ba9319ad46f508ea0ba9592a0aba3e520af0079f
ragini19854-prog <ragini.19854@gmail.com>
2026-04-23 09:35:54 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,13 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from bot.config import MONGO_URI
 
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["telegram_bot"]
+
+
+# Collections
+users_col = db["users"]
+premium_col = db["premium"]
+managers_col = db["managers"]
+blocked_col = db["blocked"]
+auth_col = db["auth"]
